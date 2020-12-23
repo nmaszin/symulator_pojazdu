ebc81dc5af75b9752a38d9f0dd2f2baba9677308
qwercik <erykandrzejewski2000@gmail.com>
2020-12-24 00:21:19 +0100

    Add base state

---
+++ src/simulation/state.js
@@ -0,0 +1,17 @@
+export default () => ({
+    velocity: {
+        current: 0,
+        integral: 0,
+        derivative: 0
+    },
+    enginePower: {
+        current: 0,
+        integral: 0,
+        derivative: 0
+    },
+    brakeStrength: {
+        current: 0,
+        integral: 0,
+        derivative: 0
+    }
+})
